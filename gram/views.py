790909718ff5563f4a63eb3a1f48b8a5bae0a781
Mwita Chacha <jonahhchacha@gmail.com>
2021-12-05 20:01:35 +0300

    Updating the index view to display users and posts

--- gram/views.py
+++ gram/views.py
@@ -10,5 +10,19 @@ from django.contrib.auth import logout# Create your views here.
 
 @login_required(login_url='/accounts/login/')
 def index(request):
+    current_user = request.user
+    images = Image.objects.all().order_by('-posted_at')
+    profiles = Profile.objects.filter(user_id = current_user.id).all()
     
-    return render(request, 'index.html')
+    if request.method == 'POST':  
+        form = CommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            com = form.save(commit=False)
+            com.user = request.user
+            com.save()
+            return redirect('index')
+    
+    else:
+        form = CommentForm()
+        
+    return render(request, 'index.html',{"current_user": current_user, "images":images, "profiles":profiles, "form":form})
